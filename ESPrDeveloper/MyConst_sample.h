4d2edfa3b9d36140e567559ac0468bd522c1e01f
Makino Kengo <ognek.m@gmail.com>
2018-12-30 13:17:18 +0900

    Add IR remote control

--- ESPrDeveloper/MyConst_sample.h
+++ ESPrDeveloper/MyConst_sample.h
@@ -3,3 +3,7 @@
 
 #define MY_THINGSPEAK_CHANNEL_ID 623320
 #define MY_THINGSPEAK_APIKEY_WRITE "your_channel_write_key"
+
+#define MY_THINGSPEAK_CHANNEL_ID_STATE 123456
+#define MY_THINGSPEAK_APIKEY_READ_STATE "your_channel_read_key"
+#define MY_THINGSPEAK_APIKEY_WRITE_STATE "your_channel_write_key"
